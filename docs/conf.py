af7bec7f6248993c1adfd2ada6d5bae86a5cfe4a
Annika <annika.rudolph@desy.de>
2023-09-24 21:13:39 +0200

    requirements updated

--- docs/conf.py
+++ docs/conf.py
@@ -69,6 +69,16 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_dark_mode'
 
+html_theme_options = {
+    "logo_only": False,
+    "display_version": False,
+    "collapse_navigation": True,
+    "navigation_depth": 4,
+    "prev_next_buttons_location": "bottom",  # top and bottom
+}
+
+html_logo = "media/logo.png"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
